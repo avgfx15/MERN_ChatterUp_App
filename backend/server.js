48df8afabfac59b0e50a273c51cefd3a4f85d816
Ajay Gandhi <avgfx15@gmail.com>
2024-03-09 07:33:26 +0530

    Dummy Data and Server api call setup for fetching data

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv'
+
+import chats from './data/data.js'
 const app = express();
 dotenv.config()
 
 app.get('/', (req, res) => {
     res.send('App is Running ')
+})
+
+app.get('/api/chat', (req, res) => {
+    res.send(chats)
+})
+
+app.get('/api/chat/:id', async (req, res) => {
+    const findChat = await chats.find(chat => chat.id === req.params.chat)
+    res.send(findChat)
 })
 
 const port = process.env.PORT || 5200;
