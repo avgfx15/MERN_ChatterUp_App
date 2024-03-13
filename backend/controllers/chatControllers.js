f948c7b5b174fd6dd32930188195de199d2d0b0d
Ajay Gandhi <avgfx15@gmail.com>
2024-03-13 08:07:53 +0530

    Create new Chat Group

--- backend/controllers/chatControllers.js
+++ backend/controllers/chatControllers.js
@@ -4,10 +4,13 @@ import UserModel from "../models/userModel.js"
 
 // @ Get All chats
 export const getAllChatsController = asyncHandler(async (req, res) => {
+    // $ Get LoggedInUser Data
     const loggedInUser = req.user._id;
     try {
+
+        // $ Get all Chats of loggedInUser with Others
         const chatOfLoggedInUser = await ChatModel.find({ users: loggedInUser }).populate('users', "-password").populate('latestChat').sort({ updatedAt: -1 });
-        res.status(200).json({ chatOfLoggedInUser })
+        return res.status(200).json({ chatOfLoggedInUser })
     } catch (error) {
         res.status(400);
         throw new Error(error.message)
@@ -34,13 +37,13 @@ export const ceateChatController = asyncHandler(async (req, res) => {
             { users: { $elemMatch: { $eq: userId } } },
         ],
     }).populate('users', "-password").populate('latestChat');
-
+    // $ Populate sender Data for latestChat
     isAlreadyInChat = await UserModel.populate(isAlreadyInChat, {
         path: "latestChat.sender",
         select: "name emial mobile profilePic"
     })
 
-    //// If not in chat, create a new chat
+    // +  If not in chat, create a new chat
     if (!isAlreadyInChat) {
         isAlreadyInChat = await ChatModel.create({
             chatName: "personal-chat",
@@ -57,6 +60,7 @@ export const ceateChatController = asyncHandler(async (req, res) => {
             throw new Error(error.message)
         }
     } else {
+        // $ Already in chatModel just send
         res.json(isAlreadyInChat);
     }
 })
@@ -65,12 +69,49 @@ export const ceateChatController = asyncHandler(async (req, res) => {
 // + Create Chat Group
 export const createChatGroupController = asyncHandler(async (req, res) => {
     try {
+        //// Get Logged User
+        const loggedInUser = req.user._id;
 
-    } catch (error) {
-        res.status(400);
-        throw new Error(error.message)
+        const { chatName } = req.body
+        // # Check for Incomplete Data
+        if (!chatName || !req.body.users) {
+            return res.status(400).send("incomplete data")
+        }
+
+        // # Check Groupname is already Exists or not
+        let existingChat = await ChatModel.findOne({ chatName })
+        if (existingChat) {
+            return res.status(400).json({ message: 'This group name has already been used.' })
+        }
+        // $ From Client side we are getting data as stringify so parse to store in Database
+        var users = JSON.parse(req.body.users);
+        // $ Atleast 2 Members require to Create Group
+        if (users.length < 2) {
+            return res.status(400).json({ message: 'More then 2 members require to Create New Group.' })
+        }
+
+        users.push(loggedInUser);  // $ add the logged user to the list of members
+
+        // + Create new Group 
+        const newGroupChat = await ChatModel.create({
+            chatName,
+            isGroupChat: true,
+            users,
+            groupAdmin: loggedInUser,
+            messages: [],
+        });
+
+        const getGroupData = await ChatModel.findOne({ _id: newGroupChat._id }).populate('users', "-password").populate('groupAdmin', "-password")
+
+        // $ send response with the newly created chat info
+        return res.status(201).json(getGroupData)
+
+    } catch (err) {
+        console.error(`Error in creating chat room : ${err}`);
+        return res.status(500).json({ error: err });
     }
-})
+});
+
 
 // * Rename Group Name
 
